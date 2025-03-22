206aa1c5111797b9cf51a956a544a76af793af9e
lazyScriptor <theekshana.jny@gmail.com>
2025-03-22 09:32:48 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://github.com/lazyScriptor/hkholdings-website-new/',
   server: {
     host: '0.0.0.0', // Allow access from outside the container
     port: 5173, // Ensure this matches the exposed port
