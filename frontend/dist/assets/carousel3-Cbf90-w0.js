881178bbdadb1da2d2d50ae155cf71058054dbdb
lazyScriptor <theekshana.jny@gmail.com>
2025-03-22 09:34:20 +0530

    build

---
+++ frontend/dist/assets/carousel3-Cbf90-w0.js
@@ -0,0 +1,1 @@
+const s="https://github.com/lazyScriptor/hkholdings-website-new/assets/carousel3-DM2jGqav.jpg";export{s as c};
---
+++ frontend/dist/assets/index-C1RwD_RH.js
@@ -0,0 +1,1 @@
+import{G as r}from"./index-Du8wxtP2.js";function o(t){return r({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"currentColor"},child:[{tag:"path",attr:{d:"M12 1L21.5 6.5V17.5L12 23L2.5 17.5V6.5L12 1ZM12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z"},child:[]}]})(t)}export{o as R};
